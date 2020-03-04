1792dc18e42e5f24e60082083f1ea0a6e192ba6d
Uogauju <jonasdavenis@gmail.com>
2020-03-04 13:03:10 +0200

    lyginimai

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const n1 = 1;
+const n2 = 2;
+const n3 = 3;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+
+const t1 = 'jonas';
+const t2 = 'davenis';
+const t3 = 'vilnius';
+
+console.log(t1);
+console.log(t2);
+console.log(t3);
+
+const pazymiai = [1, 2, 3, 4, 5];
+console.log(pazymiai);
+
+const pazymiai2 = [6, 7, 8, 9, 10];
+console.log(pazymiai2);
+
+const pazymiai3 = [11, 12, 13, 14, 15];
+console.log (pazymiai3);
+
+console.log(1 + 2 + 3);
+
+const miestai = ['vilnius', 'kaunas', 'klaipeda', 'panevezys', 'siauliai'];
+console.log(miestai);
+
+console.log(suma = t1 + t2 + t3);
+
+if (n1 < n2) {
+
+    console.log('pomidoras');
+
+} else {
+
+    console.log('bandykite dar karta');
+
+}
+if (n1 > n2) {
+
+    console.log('pomidoras');
+
+} else {
+
+    console.log('bandykite dar karta');
+
+}
