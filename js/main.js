ad479d7e1adfff32ba306408a6cbaa1e2970c7b8
Uogauju <jonasdavenis@gmail.com>
2020-03-05 19:53:52 +0200

    dienos

--- js/main.js
+++ js/main.js
@@ -1,139 +1,33 @@
-const n1 = 1;
-const n2 = 2;
-const n3 = 3;
-
-console.log(n1);
-console.log(n2);
-console.log(n3);
-
-const t1 = 'jonas';
-const t2 = 'davenis';
-const t3 = 'vilnius';
-
-console.log(t1);
-console.log(t2);
-console.log(t3);
-
-const pazymiai = [1, 2, 3, 4, 5];
-console.log(pazymiai);
-
-const pazymiai2 = [6, 7, 8, 9, 10];
-console.log(pazymiai2);
-
-const pazymiai3 = [11, 12, 13, 14, 15];
-console.log (pazymiai3);
-
-console.log(1 + 2 + 3);
-
-const miestai = ['vilnius', 'kaunas', 'klaipeda', 'panevezys', 'siauliai'];
-console.log(miestai);
-
-console.log(suma = t1 + t2 + t3);
-
-if (n1 < n2) {
-
-    console.log('pomidoras');
-
-} else {
-
-    console.log('bandykite dar karta');
-
-}
-if (n1 > n2) {
-
-    console.log('pomidoras');
-
-} else {
-
-    console.log('bandykite dar karta');
-
+for ( let i=0; i<=5; i++ ) {
+    console.log( i )
 }
 
-if (n1 >= n2) {
-
-    console.log('pomidoras');
-
-} else {
-
-    console.log('bandykite dar karta');
-
-}
-if (n1 <= n2) {
-
-    console.log('pomidoras');
-
-} else {
-
-    console.log('bandykite dar karta');
-
-}
-if (n1 == n2) {
-
-    console.log('pomidoras');
-
-} else {
-
-    console.log('bandykite dar karta');
-
-
+for ( let o=-6; o<=5; o=o+3 ) {
+    console.log(o);
 }
 
-console.log(t1.length);
-console.log(t2.length);
-console.log(t3.length);
-
-if (t1 < t2) {
-
-    console.log('pomidoras');
-
-} else {
-
-console.log('Bandykite dar karta');
-}
-if (t1 > t2) {
-
-    console.log('pomidoras');
-
-} else {
-
-console.log('Bandykite dar karta');
-
-}
-if (t1 == t2) {
-
-    console.log('pomidoras');
-
-} else {
-
-console.log('Bandykite dar karta');
-
+for ( let m=10; m>0; m-- ) {
+    console.log(m);
 }
 
-if (t1 != t2) {
-
-    console.log('pomidoras');
-
-} else {
-
-console.log('Bandykite dar karta');
-
+for ( let m=0; m<=60; m++ ) {
+    console.log( m );
 }
 
-if (t1 <= t2) {
+console.log('---------- LAIKAS');
 
-    console.log('pomidoras');
-
-} else {
-
-console.log('Bandykite dar karta');
-
+for ( let h=0; h<=12; h++ ) {
+    for ( let m=0; m<=60; m++ ) {
+        console.log( h + ':' + m );
+    }
 }
 
-if (t1 >= t2) {
+console.log('---------- DIENOS');
 
-    console.log('pomidoras');
+const dienos = [31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30];
 
-} else {
-
-console.log('Bandykite dar karta');
+for ( let m=0; m<=12; m++ ) {
+    for ( let d=1; d<=[m]; d++ ) {
+        console.log( m, + ':' + d );
+    }
 }
